f7df858504f17e400771f0cc8c4834a3a1a77dc2
Prativagautam <gautam.prativa35@gmail.com>
2025-11-02 18:28:48 +0545

    Adding login and signup plus fixing case issue

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import "../css/Navbar.css"
+import { useAuth } from '../contexts/AuthContext'
 function NavBar(){
 return <nav className="navbar">
     <div className="navbar-brand">
